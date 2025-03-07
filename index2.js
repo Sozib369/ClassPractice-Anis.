4cff8a8f931f49a0aeeeb5e2eacf41ae44e6bfff
Sozib369 <mdsozibahmed210@gmail.com>
2025-03-08 03:09:28 +0600

    Add face2 personal budget Tracker

---
+++ index2.js
@@ -0,0 +1,33 @@
+
+
+//! 🛠 Personal Budget Tracker Challenge (Phase 3 - Operators: calculate the remaining balance)
+// Challenge Overview:Create a basic personal budget tracker that allows users to enter their incomes and expenses, and then displays their remaining balance using the console. Later, we will extend this project by adding functions, arrays, objects, and DOM manipulation.
+
+// 📌 Challenge Requirements:
+/*
+Declare variables for:
+User's name (string)
+single Income amount (number)
+multiple Expense amount (number)
+Remaining balance (calculated)
+Use console.log() to:
+Display the user’s name.
+Show the income, expenses, and balance.
+Format the output neatly.
+Add comments to explain calculation
+*/
+
+// Declare variables
+
+let userName = 'MD SOZIB ISLAM';
+let income = 100000;
+let Expense = 30000;
+
+// Display the budget details in console
+
+console.log("Personal budget Tracker");
+console.log(userName);
+console.log("Income : "+income);
+console.log("Expense : "+Expense);
+console.log("blance : "+(income - Expense) +" neatly")
+
