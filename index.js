7bd6d1ad7fac7a56d6dae92f922f737a148c0f36
Sozib369 <mdsozibahmed210@gmail.com>
2025-03-08 02:20:48 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//! 🛠 Personal Budget Tracker Challenge (Phase 1 - print with Console)
+// Challenge Overview:Create a basic personal budget tracker that allows users to  displays their name, income amount and expenses amount using the console. Later, we will extend this project by adding variables, operators, functions, arrays, objects, and DOM manipulation.
+// 📌 Challenge Requirements:
+/*
+Use console.log() to:
+Display the user’s name.
+Display the income, expenses.
+Format the output neatly.
+*/
+
+// solution of Challenge 1
+
+console.log("Name : MD SOZIB ISLAM");
+console.log("Income : 50000 Bd");
+console.log("Expenses : 35000");
+console.log("Savings g: "+  (50000-35000)+" Bd");
