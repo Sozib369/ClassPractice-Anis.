ab0599cfbcea7444f157ae89e27d98c43b997d4d
Sozib369 <mdsozibahmed210@gmail.com>
2025-03-10 01:42:27 +0600

    Add some program

--- script/program/LetterGread.js
+++ script/program/LetterGread.js
@@ -1,19 +1,22 @@
 
 let num = parseFloat(prompt("Enter your Number = "));
 
-if(num>=80)
+if (num>100 || num<0)
+    
+    console.log("Invalid marks");
+else if(num >=80 && num<=100 )
     console.log("A+");
-else if(num>=75)
+else if(num>=75 && num<=79 )
     console.log("A");
-else if(num>=70)
+else if(num>=70 && num<=74)
     console.log("A-");
-else if(num>=65)
+else if(num>=65 && num<=69)
     console.log("B+");
-else if(num>=60)
+else if(num>=60 && num<=64)
     console.log("B-");
-else if(num>=50)
+else if(num>=50 && num<=58)
     console.log("C");
-else if(num>=33)
+else if(num>=33 && num<=49)
     console.log("D");
 else 
     console.log("F");
