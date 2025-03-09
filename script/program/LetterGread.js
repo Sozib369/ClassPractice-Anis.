656ce940f4ba838de54c7d0c8283fd0c01a6bfcc
Sozib369 <mdsozibahmed210@gmail.com>
2025-03-10 01:24:09 +0600

    Program GPA

---
+++ script/program/LetterGread.js
@@ -0,0 +1,19 @@
+
+let num = parseFloat(prompt("Enter your Number = "));
+
+if(num>=80)
+    console.log("A+");
+else if(num>=75)
+    console.log("A");
+else if(num>=70)
+    console.log("A-");
+else if(num>=65)
+    console.log("B+");
+else if(num>=60)
+    console.log("B-");
+else if(num>=50)
+    console.log("C");
+else if(num>=33)
+    console.log("D");
+else 
+    console.log("F");
