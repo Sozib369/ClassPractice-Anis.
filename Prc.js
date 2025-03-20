73ea99585fbae274172672d30eada40622ab5c4a
Sozib369 <mdsozibahmed210@gmail.com>
2025-03-20 07:34:06 +0600

    Add sume feature

---
+++ Prc.js
@@ -0,0 +1,199 @@
+/*
+var number = Number(prompt("Enter your Number = "))
+var 
+}
+//Add Zero feature
+var Number = Number(prompt("Enter your Number = "));
+
+if(Number > 0){
+    console.log("Positive")
+}else if(Number < 0){
+    console.log("Negative ")
+}else{
+    console.log("Zero")
+}
+     
+
+/// Use Ternary Operator 
+var Number = Number(prompt("Enter your Number = "))
+
+var Number = Number > 0? "Positive" : Number < 0 ? "Negative" : "Zero"
+
+console.log(Number)
+
+*/
+
+
+
+
+// var digit = prompt("Enter your digit = ");
+
+// switch(digit){
+//     case "0":
+//         console.log("Zero");
+//         break;
+//         case "1":
+//             console.log("One");
+//             break;
+//             case "2":
+//                 console.log("Two");
+//                 break;
+//                 case "3":
+//                     console.log("Three");
+//                     break;
+//                     case "4":
+//                         console.log("Four");
+//                         break;
+//                         case "5":
+//                             console.log("Five");
+//                             break;
+//                             case "6":
+//                                 console.log("Six");
+//                                 break;
+//                                 case "7":
+//                                     console.log("Seven");
+//                                     break;
+//                                     case "8":
+//                                         console.log("Eight");
+//                                         break;
+//                                         case "9":
+//                                             console.log("Nine");
+//                                             break;
+//                                             default:
+//                                                 console.log("Not a digit ");
+            
+// }
+
+
+
+
+
+
+//Digit to Number
+// var Number = Number(prompt("Enter your Digit = "));
+
+// if(Number==0)
+//     console.log("Zeroo");
+
+// else if(Number==1)
+//     console.log("One");
+
+// else if(Number==2)
+//     console.log("Two");
+
+// else if(Number==3)
+//     console.log("Three");
+
+// else if(Number==4)
+//     console.log("Four");
+
+// else if(Number==5)
+//     console.log("Five");
+
+// else if(Number==6)
+//     console.log("Six");
+
+// else if(Number==7)
+//     console.log("Seven");
+
+// else if(Number==8)
+//     console.log("Eight");
+
+// else if(Number==9)
+//     console.log("Nine");
+// else
+//     console.log("Not a valid ");
+
+
+
+
+
+
+// Switch keyword used for vowel and consonent
+
+// var letter = prompt("Enter any letter = ");
+
+// letter  =letter.toLowerCase();
+// switch(letter){
+//     case "a":
+//         console.log("Vowel");
+//         break;
+//     case "e":
+//         console.log("Vowel");
+//         break;
+//     case "i":
+//         console.log("Vowel");
+//         break;
+//     case "o":
+//         console.log("Vowel");
+//         break;
+//     case "u":
+//         console.log("Vowel");
+//         break;
+//     default:
+//         console.log("Consonent")
+// }
+
+
+
+
+
+// Used if else Voule or consonent
+
+// var letter = prompt("Enter any letter = ");
+// letter = letter.toLowerCase();
+
+// if(letter== "a")
+//     console.log("Voule");
+
+// else if(letter == "e")
+//     console.log("Vowel");
+// else if (letter == "i")
+//     console.log("vowel");
+// else if (letter == "0")
+//     console.log("vowel");
+// else if (letter == "u")
+//     console.log("vowel");
+// else
+//     console.log("Consonent");
+
+// Use if else value for consonent or vowel letter
+
+// var letter = prompt("Enter any Letter = ");
+
+// letter = letter.toLowerCase();
+
+// if(letter== "a")
+//     console.log("vowel");
+// else if(letter == "e")
+//     console.log("vowel");
+// else if(letter== "i")
+//     console.log("vowel");
+// else if(letter== "u")
+//     console.log("Vowel");
+// else
+// console.log("consonent");
+
+
+
+ 
+
+// 
+
+
+// for( var x = 1; x<=99; x = x+2 ){
+    
+    
+//     document.writeln(" "+x);
+
+// }
+// document.writeln("End ");
+
+
+
+
+// 
+for(var x = 10; x >= 1; x = x-2 ){
+    document.writeln(" "+ x)
+}
+document.writeln("<h1>End</h1>");
